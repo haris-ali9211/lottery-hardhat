24b512e4a4d3e36e41056589b37590f9366b5e0a
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-01-07 17:42:19 +0500

    updatehepler

--- deploy/00-deplay-mocks.js
+++ deploy/00-deplay-mocks.js
@@ -1,13 +1,27 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts;
-    const chainId =network.config.chainId;
+    // const chainId = network.config.chainId;
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
-    if(developmentChains.includes(network.name)){
+
+    if (developmentChains.includes(network.name)) {
         log("local network detected! Deploying mocks....!")
         //deploy a mocks vrfCoordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args
+        })
+        log("Mocks deployed!")
+        log("-----------------------------------------------------------")
     }
 }
+
+module.exports.tags = ["all","mocks"]
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,29 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts;
 
+    let vrfCoordinatorV2Address
+    const chainId = network.config.chainId;
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mocks = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mocks.address;
+    }
+    else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const entranceFee = network.networkConfig[chainId]["entranceFee"]
+    const gasLane = network.networkConfig[chainId]["gasLane"]
+
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane]
+
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
