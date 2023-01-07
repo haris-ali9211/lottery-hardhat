9ce2f1a561e561faaf67bb3d4a6180c9a31e5393
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-01-07 16:15:27 +0500

    deploy mock

---
+++ deploy/00-deplay-mocks.js
@@ -0,0 +1,13 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts;
+    const chainId =network.config.chainId;
+
+    if(developmentChains.includes(network.name)){
+        log("local network detected! Deploying mocks....!")
+        //deploy a mocks vrfCoordinator
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,31 @@ require("dotenv").config()
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    }
+  }
 };
