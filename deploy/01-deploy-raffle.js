2c0149aa14160ca5eec5d85d1e853a2d0b0d3bf0
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-01-17 19:17:17 +0500

    failed in args

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -2,12 +2,14 @@ const { network, ethers } = require("hardhat");
 const { deveplomentChains, networkConfig } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("1");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
+    // const chainId = network.config.chainId;
+
     let vrfCoordinatorV2Address, subscriptionId;
     if(deveplomentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
@@ -27,13 +29,21 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const gasLane = networkConfig[chainId]["gasLane"]
     const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
     const interval = networkConfig[chainId]["interval"]
-    const args = [vrfCoordinatorV2Address,entranceFees,gasLane,subscriptionId,callbackGasLimit,interval]
+    // const args = [vrfCoordinatorV2Address,entranceFees,gasLane,subscriptionId,callbackGasLimit,interval]
+    const args = [
+        networkConfig[chainId]["raffleEntranceFee"],
+        vrfCoordinatorV2Address,
+        networkConfig[chainId]["gasLane"],
+        subscriptionId,
+        networkConfig[chainId]["callbackGasLimit"],
+        networkConfig[chainId]["keepersUpdateInterval"],
+        networkConfig[chainId]["interval"]
+    ]
 
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
         log: true,
-        waitConfirmations: network.config.blockConfirmations || 1
     })
 
     if(!deveplomentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,15 +1,15 @@
 const { ethers } = require("hardhat");
 
 const networkConfig = {
-    4: {
-        name : "rinkeby",
-        vrfCoordinatorV2 : "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        entranceFees : ethers.utils.parseEther("0.01"),
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", //keyhash
-        subscriptionId : "7025",
-        callbackGasLimit : "500000",
-        interval : 30
-    },
+    // 4: {
+    //     name : "rinkeby",
+    //     vrfCoordinatorV2 : "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    //     entranceFees : ethers.utils.parseEther("0.01"),
+    //     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", //keyhash
+    //     subscriptionId : "7025",
+    //     callbackGasLimit : "500000",
+    //     interval : 30
+    // },
     31337: {
         name : "localhost", 
         entranceFees : ethers.utils.parseEther("0.01"),
@@ -20,12 +20,12 @@ const networkConfig = {
     },
     5: {
         name: "goerli",
-        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-        entranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         subscriptionId: "8689",
-        callBackGasLimit: "500000",//500,000
-        interval: "30"
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     }
 
 }
