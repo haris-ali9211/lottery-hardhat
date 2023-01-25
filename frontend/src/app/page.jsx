292c5529137c08e8546f81582cd7107476a61ac4
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-01-26 00:24:43 +0500

    add  moralis

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import Header from  '../components/Header'
+
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ()=>{
+    return(
+        <div>
+            hello from Header
+        </div>
+    )
+}
+
+export default Header
